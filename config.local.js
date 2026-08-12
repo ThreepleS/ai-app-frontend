a7a624effd2765ffef63bb432f107619c7c20d92
ThreepleS <makienkokill@gmail.com>
2026-08-13 00:22:48 +0600

    add config.local.js for GOOGLE_CLIENT_ID

---
+++ config.local.js
@@ -0,0 +1,4 @@
+// Локальный конфиг (не коммитить в git).
+// Для GitHub Pages можно создать этот файл вручную на сервере,
+// либо вставить значение прямо в app.js перед деплоем.
+window.GOOGLE_CLIENT_ID = "688453339516-n83aael6s514cfk93n7mrsna07b1i5bk.apps.googleusercontent.com";
